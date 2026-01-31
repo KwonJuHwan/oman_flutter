fa545f8ca08e2680cf990507b1ff679c42dff717
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-31 20:27:21 +0900

    feat: implement app_colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 메인 브랜드 컬러
+  static const Color primaryOrange = Colors.orange;
+  static const Color primaryGreen = Colors.green;
+  
+  // 배경 및 텍스트 컬러
+  static const Color background = Color(0xFFF5F7FA);
+  static const Color white = Colors.white;
+  static const Color greyOutline = Color(0xFFE0E0E0);
+  static const Color textGrey = Color(0xFF9E9E9E);
+}
